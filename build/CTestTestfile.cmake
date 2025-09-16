0b7fefd8f20d78683918a882120f352e47797bf0
kikifachrizi <kikifachrizi235@gmail.com>
2025-09-16 19:02:40 +0700

    push tifav0.2 new update

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tifa/catkin_ws/src/tifa_control
-# Build directory: /home/tifa/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
